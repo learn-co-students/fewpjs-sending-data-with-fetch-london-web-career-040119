0b3aebf9def162074c908acde3dc0818f4eb74e3
0118ayush <ayush_gehlot@hotmail.com>
2019-05-18 16:41:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const url = "http://localhost:3000/users"
+
+function submitData(nameString, emailString){
+    
+    let formData = {
+        name: `${nameString}`,
+        email: `${emailString}`
+    }
+
+    let configData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", 
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+
+    fetch(url, configData)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(object){
+           document.body.innerHTML = object['id']
+
+
+        })
+        .catch(function(error){
+            document.body.innerHTML = error['message']
+        })
+}
